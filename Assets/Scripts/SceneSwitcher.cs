0edd7638578a7276159c7dfc4e8f48b1747adad6
ntacv <ntackn1@gmail.com>
2023-12-10 01:20:49 +0100

    First 2h for testing
    page switching
    button interaction
    AR object
    Build apk

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Next(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void Back(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
