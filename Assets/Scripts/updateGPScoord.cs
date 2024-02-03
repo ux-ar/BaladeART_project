9ea8862ce305212c6243206ff1e36c575a048a6a
ntacv <ntackn1@gmail.com>
2024-02-03 17:37:02 +0100

    variables to PlayerPrefs

--- Assets/Scripts/updateGPScoord.cs
+++ Assets/Scripts/updateGPScoord.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+using TMPro;
+
 public class updateGPScoord : MonoBehaviour
 {
     public Text coordinates;
 
+    public TextMeshProUGUI full_distance;
+
+    Toggle_element Toggle_element = new Toggle_element();
+
     private void Update()
     {
         coordinates.text = "Lat: " + GPS.Instance.latitude.ToString() + "     Lon: " + GPS.Instance.longitude.ToString();
+
+        full_distance.text = PlayerPrefs.GetInt("Distance").ToString();
 
     }
 }
